39d79113f34a104fe1eea86f2c4b0f683f10bfad
Piotr Esden-Tempski <piotr@esden.net>
2018-08-26 15:27:19 -0700

    Add a simple PWM LED example.

---
+++ pwm_fade/pwm.v
@@ -0,0 +1,84 @@
+/*
+ *  icebreaker examples - pwm demo
+ *
+ *  Copyright (C) 2018 Piotr Esden-Tempski <piotr@esden.net>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+// This example generates PWM to fade LEDs
+// The intended result is opposite pulsating Red and Green LEDs
+// on the iCEBreaker. The intended effect is that the two LED "breathe" in
+// brigtness up and down in opposite directions.
+
+module top (
+	input CLK,
+	output LEDR_N,
+	output LEDG_N,
+	output P1A7, // Debug pins 
+	output P1A8  // 
+);
+
+// PWM generator
+reg [15:0] pwm_counter = 0;
+reg [15:0] pwm_compare = 256;
+reg pwm_out;
+always @(posedge CLK) begin
+	// Divide clock by 65535
+	// Results in a 183.11Hz PWM
+	pwm_counter <= pwm_counter + 1;
+
+	// Set pwm output according to the compare
+	// Set output high when the counter is smaller than the compare value
+	// Set output low when the counter is equal or higher than the compare
+	// value
+	if (pwm_counter < pwm_compare) begin
+		pwm_out <= 1;
+	end else begin
+		pwm_out <= 0;
+	end
+end
+
+// PWM compare generator
+// Fading up and down creating a slow sawtooth output
+// The fade up down takes about 11.18 seconds
+// Note: You will see that the LEDs spend more time being very bright
+// than visibly fading, this is because our vision is non linear. Take a look
+// at the pwm_fade_gamma example that fixes this issue. :)
+reg [16:0] pwm_inc_counter = 0;
+reg pwm_dir = 1;
+localparam pwm_increment = 16'b1000_0000;
+always @(posedge CLK) begin
+	// Divide clock by 131071
+	pwm_inc_counter <= pwm_inc_counter + 1;
+
+	// increment/decrement pwm compare value at 91.55Hz
+	if (pwm_inc_counter == 0)
+		if (pwm_dir)
+			pwm_compare <= pwm_compare + pwm_increment;
+		else
+			pwm_compare <= pwm_compare - pwm_increment;
+
+	if (pwm_compare >= 16'hFF00)
+		pwm_dir <= 0;
+	if (pwm_compare == 0)
+		pwm_dir <= 1;
+end
+
+assign LEDG_N = ~pwm_out;
+assign LEDR_N =  pwm_out;
+assign P1A7 = pwm_counter[15]; // 50% duty cycle PWM clock out
+assign P1A8 = pwm_out; // PWM output on a GPIO pin
+
+endmodule
